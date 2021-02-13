bee7780fa61bc72a5dd49165ad771139e6abfec2
modernsilpi <modernsilpi@gmail.com>
2021-02-13 09:08:06 +0530

    chat

--- src/components/chats-section.js
+++ src/components/chats-section.js
@@ -26,15 +26,15 @@ useEffect(()=>{
   firebase.auth().onAuthStateChanged(function(user) {
     if(user) {
 
-
+var temp=[];
       db.collection("messaging").where("userid", "==", firebase.auth().currentUser.uid)
       .get()
       .then((querySnapshot) => {
           querySnapshot.forEach((doc) => {
-            
-              setchit(doc.data())
+              temp.push(doc.data())
+              // setchit(doc.data())
           });
-      })
+      }).then(()=>setchit(temp))
       .catch((error) => {
           console.log("Error getting documents: ", error);
       });
@@ -46,13 +46,14 @@ return chit
 const Sekhar=()=> {
 
 const chit=useTimes()
+console.log(chit)
 if(chit.body){
 console.log(chit)}
         return(
          
         <div className="responses">
                 <Headings />
-                {chit.body  
+                {chit  
                  ?<Mybookings data={chit}/> 
                 :<Empty />
                 }
@@ -62,47 +63,39 @@ console.log(chit)}
 
 export default Sekhar;
 
-function Empty(){
-  return <div></div>
-}
+
 
 function Mybookings(props) {
   const history = useHistory();
+const[chat,setchat]=useState([]);
 
-
-console.log(props.data)
+  const click =(prop)=>{
+    console.log("click",prop)
+    db.collection('messaging').doc(prop).onSnapshot(snap=>{
+      setchat(snap.data())
+    })
+  }
 
 
       return (<div>
           <Row style={{margin: "0"}}>
-  {/* props.data.map((cap)=> */}
   <Col lg="3" style={{margin: "0", padding: "0"}}>
 <div style={{width: "100%", borderRight: "1px solid black", height: "600px", overflow: "auto"}}>
-  
+  { props.data.map((nap)=>
   <div style={{display: "inline-flex", width: "100%"}}>
-      <img className="chat-pic" style={{width: "30px", height: "30px", borderRadius: "50%"}} src={"pic"} />
-      <h3>Hemanth</h3>
+      <img className="chat-pic" style={{width: "30px", height: "30px", borderRadius: "50%"}} src={nap.ppic} />
+      <h3 id={nap.id} onClick={(e)=>click(nap.id)}>{nap.pname}</h3>
   </div>
-  <div style={{display: "inline-flex"}}>
-     <img className="chat-pic"  style={{width: "30px", height: "30px", borderRadius: "50%"}} src={"pic"} />
-     <h3>Sekhar</h3>
-  </div>
+  )}
  
 </div>
 </Col>
 <Col lg="9">
 <div>
-
-
-    < Chatarea chat={props.data}/>
-<Form.Group style={{position: "absolute", bottom: "2px",width: "98%", margin: "0"}}>
-    <Row style={{margin: "0"}}>
-        <Col lg="10">
-  <Form.Control type="text" placeholder="Message Here" /></Col>
-  <Col lg="2">
-  <Button variant="outline-primary" className="chatSend">Send <MdSend /></Button>{' '}</Col>
-  </Row>
-</Form.Group>
+{chat.body
+    ?< Chatarea chat={chat}/>
+    :<Empty />
+}
 </div>
 </Col>
 </Row>
@@ -114,6 +107,15 @@ console.log(props.data)
 function Chatarea(props){
   let chat=[]
   chat=props.chat;
+
+  const click =(prop)=>{
+    console.log("click",prop)
+    let msg=document.getElementById('msgtext');
+    db.collection('messaging').doc(prop).update({
+      body:firebase.firestore.FieldValue.arrayUnion(msg.value)
+    }).then(()=>{msg.value=''})
+  }
+
 return(
   <div style={{float: "right", width: "50%"}}>
     {
@@ -121,6 +123,14 @@ chat.body.map((nap)=>
     
   <p className="chatList" style={{listStyle: "none", textAlign: "right", marginTop: "10px", background: "#faf3e0", borderRadius: "20px", fontSize: "20px", padding: "6px"}}>{nap}</p>
 )}
+<Form.Group style={{position: "absolute", bottom: "2px",width: "98%", margin: "0"}}>
+    <Row style={{margin: "0"}}>
+        <Col lg="10">
+  <Form.Control type="text" placeholder="Message Here" id="msgtext"/></Col>
+  <Col lg="2">
+  <Button variant="outline-primary" className="chatSend" id={props.chat.id} onClick={(e)=>click(props.chat.id)}>Send <MdSend /></Button>{' '}</Col>
+  </Row>
+</Form.Group>
 </div>
 )
 }
@@ -138,8 +148,10 @@ chat.body.map((nap)=>
   }
 
  
-firebase.auth().onAuthStateChanged(function(user) {
-  if(user) {
 
-  }
-})
+
+
+
+function Empty(){
+  return <div></div>
+}
