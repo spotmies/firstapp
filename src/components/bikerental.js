793a69dc98b3160c5c603502bd019e86e6bd37e0
moernsilpi <modernsilpi@gmail.com>
2021-02-15 18:26:27 +0530

    book

--- src/components/bikerental.js
+++ src/components/bikerental.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import 'semantic-ui-css/semantic.min.css'
-import { Card, Icon,Button,Header, Image, Modal } from 'semantic-ui-react'
+import { Card, Icon,Button,Header, Image, Modal,Step,Menu } from 'semantic-ui-react'
 import '../rental.css';
 import {SiCoronaengine} from 'react-icons/si'
 import {BsFillGearFill} from 'react-icons/bs'
@@ -16,19 +16,20 @@ const db=firebase.firestore();
 function Rentals(){
   const times=useTimes()
   const [open, setOpen] = useState(false)
+  const [col,setcol]=useState('primary')
 
     return <div>
         <div>
             <div style={{padding:"10px",marginLeft:"auto",marginRight:"auto"}}>
             <Card.Group>     
                 {times.map((nap,key)=>     
-  <Card className="rentcard" id={nap.id} key={key} style={{width:'350px'}}> 
+  <Card className="rentcard" id={nap.id} key={key} style={{width:'370px'}}> 
     <Image src={nap.photo} wrapped ui={true} />
     <Card.Content>
       <Card.Header style={{textAlign:"center"}}>{nap.model}</Card.Header>
 
       <Card.Description>
-     <b> &#8377; {nap.price}/Day</b>
+     {/* <b> &#8377; {nap.price}/Day</b> */}
       </Card.Description>
     </Card.Content>
     <Card.Content extra>
@@ -53,8 +54,17 @@ function Rentals(){
         &nbsp;{nap.kmslimit} km
         </a></p>
       </div>
+      <div>
+      <Button.Group style={{width:'320px',marginTop:'10px'}}>
+    <Button primary> &#8377;{nap.price*50} <br /> <small> 50km  </small></Button>
+
+    <Button> &#8377;{nap.price*100}<br /> <small> 100km </small></Button>
+    <Button>&#8377;{nap.price*150} <br />  <small> 150km </small></Button>
+  </Button.Group>
+
+      </div>
     </Card.Content>
-    <Button id={nap.id} onClick={(e)=>setOpen(true)}>Book now</Button>
+    <Button id={nap.id} color="blue" onClick={(e)=>setOpen(true)}>Book now</Button>
     <div>
     <Modal
       onClose={() => setOpen(false)}
@@ -64,11 +74,11 @@ function Rentals(){
       
       <Modal.Header>{nap.model}</Modal.Header>
       <Modal.Content image>
-        <Image size='medium' src={nap.photo} wrapped />
+        <Image size='medium' src={nap.photo} wrapped/>
         <Modal.Description>
           <Header><h1>{nap.model}</h1></Header>
           <p>
-          <b> &#8377; {nap.price}/Day</b>
+          {/* <b> &#8377; {nap.price}/Day</b> */}
           </p>
         <div style={{display:'inline-flex'}}> 
         <p><a>
@@ -76,36 +86,57 @@ function Rentals(){
         &nbsp; {nap.toc} 
         </a></p>
 
-        <p style={{marginLeft:"10px"}}><a>
+        <p style={{marginLeft:"20px"}}><a>
         <BsFillGearFill />
         &nbsp; {nap.geartype} 
         </a></p>
 
-        <p style={{marginLeft:"10px"}}> <a>
+        <p style={{marginLeft:"20px"}}> <a>
         <Icon name='wheelchair' />
         {nap.seats}seats
       </a></p>
 
-      <p style={{marginLeft:"10px"}}><a>
+      <p style={{marginLeft:"20px"}}><a>
         <IoSpeedometerOutline />
         &nbsp;{nap.kmslimit} km
         </a></p>
 
         {nap.fuelincl
-        ?<p style={{marginLeft:"10px"}}> <Icon name="tint" />fuel: include</p>
-        :<p style={{marginLeft:"10px"}}> <Icon name="tint" />fuel: exclude</p>
+        ?<p style={{marginLeft:"20px"}}> <Icon name="tint" />fuel: include</p>
+        :<p style={{marginLeft:"20px"}}> <Icon name="tint" />fuel: exclude</p>
         }
+
+        </div>
+        <div style={{display:"inline-flex"}}>
         {nap.delivery
-        ?<p style={{marginLeft:"10px"}}><FaPeopleCarry />&nbsp;delivery: yes</p>
-        :<p style={{marginLeft:"10px"}}><FaPeopleCarry />delivery: no</p>
+        ?<p><FaPeopleCarry />&nbsp;door delivery: yes</p>
+        :<p style={{marginLeft:"20px"}}><FaPeopleCarry />delivery: no</p>
         }
+        <p style={{marginLeft:"20px"}}><a>
+        <IoSpeedometerOutline />
+        &nbsp;{nap.maxspeed} kmph
+        </a></p>
+
+        <p style={{marginLeft:"20px"}}><a>
+        <IoSpeedometerOutline />
+        &nbsp;Model by: {nap.carcomp}
+        </a></p>
         </div>
         <p>
           {nap.descr}
         </p>
         <div>
+      <Button.Group style={{width:'320px',marginTop:'10px'}}>
+    <Button> &#8377;{nap.price*50} <br /> <small> 50km  </small></Button>
 
-        </div>
+    <Button> &#8377;{nap.price*100}<br /> <small> 100km </small></Button>
+    <Button>&#8377;{nap.price*150} <br />  <small> 150km </small></Button>
+  </Button.Group>
+
+      </div>
+      <div>
+
+      </div>
         </Modal.Description>
       </Modal.Content>
       <Modal.Actions>
@@ -182,19 +213,23 @@ function Rentals(){
 function useTimes(){
   const[times,setTimes]=useState([])
   useEffect(()=>{
+    var newtimes;
     db.collection('rentals').get().then((snap)=>{
       snap.docs.forEach(nap=>{
         if(nap.data().permission){
           db.collection('rentals').doc(nap.id).collection('products').get().then(snap=>{
-            const newtimes=snap.docs.map((doc)=>({
+            newtimes=snap.docs.map((doc)=>({
               id:doc.id,
               ...doc.data()
             }))
-            setTimes(newtimes);
+             setTimes(newtimes);
             
           })
         }
       })
+    })
+    .then(()=>{
+     // setTimes(newtimes);
     })
   },[])
   return times;
