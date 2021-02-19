728c4af4c731327a5e91fb02835a36bab1662f11
modernsilpi <modernsilpi@gmail.com>
2021-02-19 08:30:14 +0530

    dfg

--- src/components/mybooks.js
+++ src/components/mybooks.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import firebase from '../firebase';
 import react,{useState,useEffect} from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Card } from 'react-bootstrap';
+//import {Card } from 'react-bootstrap';
 import '../index.css';
 import { useHistory } from 'react-router-dom'
 
+import 'semantic-ui-css/semantic.min.css'
+import { Card, Icon,Button,Header, Image, Modal,Step,Menu,Dropdown } from 'semantic-ui-react'
+
+
 //import icons
+import {IconContext} from 'react-icons';
 import {BsEyeFill} from 'react-icons/bs';
 import {BiTimeFive} from 'react-icons/bi';
 import {RiPinDistanceFill} from 'react-icons/ri'
@@ -70,10 +75,10 @@ const delpost=(pro)=>{
 }
       return <div>
         
-{
+{/* {
   props.data.map((cap)=>
 <div id={cap.id} style={{marginLeft:"22%"}}>
-  
+<IconContext.Provider value={{ size:"1.5em"}}>
   <Card 
   bg="primary"
   key="2"
@@ -82,25 +87,56 @@ const delpost=(pro)=>{
   className="mb-2 "
 >
 
- <Card.Header >{cap.problem}</Card.Header>
+ <Card.Header style={{display:'inline-flex'}}>{String(cap.posttime.toDate()).replace('GMT+0530 (India Standard Time)','')}
+ <AiFillEdit  color="gray"  onClick={(e)=>edit(cap.orderid)} />
+ <MdDelete  color="red"  onClick={(e)=>delpost(cap.orderid)} />
+
+ </Card.Header>
   <Card.Body className="card-body">
     <Card.Title>
     <img className="post-img" src={cap.media[0]} alt="" />
    <h3 className="post-title"  value="sekhar">{cap.problem}</h3> 
    <h4 onClick={(e)=>click(cap.id)}>view post</h4>
    <div className="details-post">
-   <p><BsEyeFill /> Views: {cap.views}</p>
+   <p><BsEyeFill />&nbsp; Views: {cap.views}</p>
    <p><RiPinDistanceFill /> Distance: 105</p>
    <p><HiOutlineCurrencyRupee /> Money: {cap.price}</p>
    <p><BiTimeFive /> Time: 1hr</p>
-   <p onClick={(e)=>edit(cap.orderid)}><AiFillEdit />edit</p>
-   <p onClick={(e)=>delpost(cap.orderid)}><MdDelete />delete</p>
+   <p ></p>
+
    </div>
     </Card.Title>
   </Card.Body>
 </Card>
+</IconContext.Provider>
 </div>
-  )}
+  )} */}
+  <Card.Group>
+    <Card>
+      <Card.Content>
+        <Image
+          floated='right'
+          size='mini'
+          src='https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW58ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80'
+        />
+        <Card.Header style={{color:"red"}}>Steve Sanders <MdDelete /></Card.Header>
+        <Card.Meta>Friends of Elliot</Card.Meta>
+        <Card.Description>
+          Steve wants to add you to the group <strong>best friends</strong>
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <div className='ui two buttons'>
+          <Button basic color='green'>
+            Approve
+          </Button>
+          <Button basic color='red'>
+            Decline
+          </Button>
+        </div>
+      </Card.Content>
+    </Card>
+    </Card.Group>
       </div>
       
  
