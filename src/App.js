b0da9d6a6323c336e6987d4f66c449ea59844384
modernsilpi <modernsilpi@gmail.com>
2021-04-17 11:40:24 +0530

    unused files deleted

--- src/App.js
+++ src/App.js
@@ -4,14 +4,10 @@ import { Switch, Route,BrowserRouter } from "react-router-dom";
 import Navibar from "./components/navbar";
 import Firstslide from "./components/slide1";
 import postview from "./components/postview";
-import Product from "./components/product";
-import productlist from "./components/productlist";
 import Mybooks from "./components/mybooks";
-import newpost from "./components/newpost";
 import chat from "./components/chat";
 import signup from "./components/signup";
 import profile from './components/profile';
-import Editpost from './components/editpost';
 import SimpleMap from './components/contact';
 import chatssection from "./components/chats-section";
 import CardExampleCard from './components/bikerental';
@@ -19,7 +15,7 @@ import pdetails from './components/pdetails';
 import newpost2 from './components/newpost2';
 import editpost2 from './components/editpost2'
 import nav2 from './components/nav2'
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import partnerRegistration from './components/partnerRegistration';
 import scroll from './components/scroll';
@@ -33,15 +29,12 @@ return(<>
       <Route exact path="/firstapp" component={Firstslide} />
       <Route exact path="/mybookings"  ><Mybooks />
       </Route>
-      <Route path="/cart" component={productlist}/>
       <Route path="/newpost" component={newpost2}/>
-      {/* <Route path="/newpost2" component={newpost2}/> */}
       <Route path="/chat" component={chat}/>
       <Route path="/chats-section" component={chatssection}/>
       <Route path="/signup" component={signup}/>
       <Route path="/signup/home" component={Firstslide}/>
       <Route path="/account" component={profile}/>
-      {/* <Route path="/mybookings/id/edit" component={Editpost}/> */}
       <Route path="/mybookings/id/edit" component={editpost2}/>
       <Route path="/mybookings/id" component={postview}/>
       <Route path="/contact" component={SimpleMap}/>
