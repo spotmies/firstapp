329ef472fe8d8fd261fa5e6959a4d0b6e4ddc138
modernsilpi <modernsilpi@gmail.com>
2021-05-08 10:48:37 +0530

    feedback completed

--- src/components/slide1.jsx
+++ src/components/slide1.jsx
@@ -2,15 +2,18 @@ import "../App.css";
 import repair from "../images/repair.svg";
 import { Link } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
-import { Modal } from "semantic-ui-react";
+import { Modal, Form, Button, Header, TextArea } from "semantic-ui-react";
 import React, { useState, useEffect, useRef, useCallback } from "react";
+//toast
+import { toast } from "react-toastify";
+
 import {
   Dropdown,
   DropdownButton,
   // Modal,
-  Button,
+  // Button,
   InputGroup,
-  Form,
+  // Form,
   Col,
   Row,
   FormControl,
@@ -18,7 +21,14 @@ import {
 } from "react-bootstrap";
 import { BiRupee } from "react-icons/bi";
 import { BsTools, BsFillLockFill, BsFillUnlockFill } from "react-icons/bs";
-import { MdFeedback } from "react-icons/md";
+import {
+  MdFeedback,
+  MdHelp,
+  MdKeyboardArrowLeft,
+  MdSentimentSatisfied,
+  MdThumbDown,
+  MdThumbUp,
+} from "react-icons/md";
 import { FaGooglePlay } from "react-icons/fa";
 import firebase from "../firebase";
 import Zoom from "react-reveal/Zoom";
@@ -41,6 +51,8 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 // import "animate.css/animate.min.css";
 import ScrollAnimation from "react-animate-on-scroll";
 
+//import db files
+import { feedBack1 } from "../mservices/contactUs";
 // gsap.registerPlugin(ScrollTrigger);
 
 var newpost = "/signup";
@@ -142,6 +154,10 @@ function Slide() {
     // window.location.href = 'https://modernsilpi.com';
     window.open("https://modernsilpi.com", "_blank");
   };
+
+  const closeModal = () => {
+    setOpen(false);
+  };
   // let location="seethamadahar";
   const db = firebase.firestore();
   // const [modalShow, setModalShow] = React.useState(false);
@@ -203,8 +219,8 @@ function Slide() {
         <div className="feedBack " onClick={() => setOpen(true)}>
           <MdFeedback className="feedBackIcon" />
         </div>
-
-        <Modal
+        <ModalExampleModal open={open} close={closeModal} />
+        {/* <Modal
           className="fbModal"
           onClose={() => setOpen(false)}
           onOpen={() => setOpen(true)}
@@ -379,7 +395,7 @@ function Slide() {
               Submit{" "}
             </Button>
           </Modal.Actions> */}
-        </Modal>
+        {/* </Modal> */}
 
         <section className="LockPsw">
           <Fade top>
@@ -528,8 +544,8 @@ function Slide() {
         <div className="feedBack" onClick={() => setOpen(true)}>
           <MdFeedback className="feedBackIcon" />
         </div>
-
-        <Modal
+        <ModalExampleModal open={open} close={closeModal} />
+        {/* <Modal
           className="fbModal"
           onClose={() => setOpen(false)}
           onOpen={() => setOpen(true)}
@@ -704,7 +720,7 @@ function Slide() {
               Submit{" "}
             </Button>
           </Modal.Actions> */}
-        </Modal>
+        {/*    </Modal> */}
 
         <section className="LockPsw">
           <Fade top>
@@ -777,3 +793,276 @@ function Slide() {
 }
 
 export default Slide;
+
+function ModalExampleModal(props) {
+  const [open, setOpen] = useState(false);
+  const [count, setcount] = useState(0);
+  const [que, setque] = useState([]);
+
+  useEffect(() => {
+    setOpen(props.open);
+    setcount(0);
+    setque([]);
+  }, [props.open]);
+
+  useEffect(() => {
+    if (count == 10) {
+      setTimeout(() => {
+        onClose();
+      }, 1000);
+    }
+  }, [count]);
+
+  const onClose = useCallback(() => {
+    props.close();
+  });
+
+  const nextQue = (ans, index) => {
+    setcount(index + 1);
+    let tempque = que;
+    tempque[index] = ans;
+    setque(tempque);
+  };
+
+  const prevQue = () => {
+    if (count != 0) setcount(count - 1);
+  };
+
+  const textArea = (e, index) => {
+    // console.log(e);
+    let ans = e.target.value;
+    let tempque = que;
+    tempque[index] = ans;
+    setque(tempque);
+  };
+
+  const formSubmit = async () => {
+    console.log(que);
+    var obj = {
+      q0: que[0],
+      q1: que[1],
+      q2: que[2],
+      q3: que[3],
+      q4: que[4] == undefined ? "" : que[4],
+    };
+    console.log(obj);
+    console.log(JSON.stringify(obj));
+    var strobj = JSON.stringify(obj);
+    let result = await feedBack1(strobj);
+    if (result == 200) {
+      toast.success("Thanks For Your Feedback");
+      setcount(10);
+    } else {
+      toast.error("Something went wrong please try again");
+      onClose();
+    }
+  };
+
+  return (
+    <Modal
+      className="fbModal"
+      onClose={onClose}
+      onOpen={() => setOpen(true)}
+      open={open}
+      size="tiny"
+      // trigger={<Button>Show Modal</Button>}
+    >
+      <Modal.Header>Feedback Here</Modal.Header>
+      <Modal.Content>
+        {count > 0 ? (
+          <MdKeyboardArrowLeft size="2rem" onClick={prevQue} />
+        ) : null}
+        <Modal.Description>
+          {count == 0 ? (
+            <div>
+              <Header>Do You Understand What Does This Website Means</Header>
+              <div>
+                <Button
+                  basic
+                  color={que[0] == "yes" ? "blue" : "grey"}
+                  onClick={() => {
+                    nextQue("yes", 0);
+                  }}
+                >
+                  <MdThumbUp /> Yeah
+                </Button>
+                <Button
+                  basic
+                  color={que[0] == "no" ? "blue" : "grey"}
+                  onClick={() => {
+                    nextQue("no", 0);
+                  }}
+                >
+                  <MdThumbDown /> Nope
+                </Button>
+                <Button
+                  basic
+                  color={que[0] == "no idea" ? "blue" : "grey"}
+                  onClick={() => {
+                    nextQue("no idea", 0);
+                  }}
+                >
+                  <MdHelp /> No Idea
+                </Button>
+              </div>
+            </div>
+          ) : null}
+
+          {count == 1 ? (
+            <div>
+              <Header>Is This Platform Is Useful</Header>
+              <div>
+                <Button
+                  basic
+                  color={que[1] == "yes" ? "blue" : "grey"}
+                  onClick={() => {
+                    nextQue("yes", 1);
+                  }}
+                >
+                  <MdThumbUp /> Yes
+                </Button>
+                <Button
+                  basic
+                  color={que[1] == "no" ? "blue" : "grey"}
+                  onClick={() => {
+                    nextQue("no", 1);
+                  }}
+                >
+                  <MdThumbDown /> No
+                </Button>
+                <Button
+                  basic
+                  color={que[1] == "maybe" ? "blue" : "grey"}
+                  onClick={() => {
+                    nextQue("maybe", 1);
+                  }}
+                >
+                  <MdSentimentSatisfied /> Maybe
+                </Button>
+                <Button
+                  basic
+                  color={que[1] == "i don't know" ? "blue" : "grey"}
+                  onClick={() => {
+                    nextQue("i don't know", 1);
+                  }}
+                >
+                  <MdHelp /> I Don't Know
+                </Button>
+              </div>
+            </div>
+          ) : null}
+
+          {count == 2 ? (
+            <div>
+              <Header>How This Website Looks</Header>
+              <div>
+                <Button
+                  basic
+                  color={que[2] == "good" ? "blue" : "grey"}
+                  onClick={() => {
+                    nextQue("good", 2);
+                  }}
+                >
+                  <MdThumbUp /> Looks Good
+                </Button>
+                <Button
+                  basic
+                  color={que[2] == "its ok" ? "blue" : "grey"}
+                  onClick={() => {
+                    nextQue("its ok", 2);
+                  }}
+                >
+                  <MdSentimentSatisfied /> It's Ok
+                </Button>
+                <Button
+                  basic
+                  color={que[2] == "bad" ? "blue" : "grey"}
+                  onClick={() => {
+                    nextQue("bad", 2);
+                  }}
+                >
+                  <MdThumbDown /> Looks Bad
+                </Button>
+                <Button
+                  basic
+                  color={que[2] == "i don't know" ? "blue" : "grey"}
+                  onClick={() => {
+                    nextQue("i don't know", 2);
+                  }}
+                >
+                  <MdHelp /> I Don't Know
+                </Button>
+              </div>
+            </div>
+          ) : null}
+
+          {count == 3 ? (
+            <div>
+              <Header>Have You Face Any Issue With The Website</Header>
+              <div>
+                <Button
+                  basic
+                  color={que[3] == "no" ? "blue" : "grey"}
+                  onClick={() => {
+                    nextQue("no", 3);
+                  }}
+                >
+                  <MdThumbUp /> No
+                </Button>
+                <Button
+                  basic
+                  color={que[3] == "yes" ? "blue" : "grey"}
+                  onClick={() => {
+                    nextQue("yes", 3);
+                  }}
+                >
+                  <MdThumbDown /> Yes
+                </Button>
+              </div>
+            </div>
+          ) : null}
+
+          {count == 4 ? (
+            <div>
+              <Header>
+                If You Have Any Other Feedback Please Tell Us Here. We Love To
+                Improve Our Service
+              </Header>
+              <div>
+                <Form>
+                  <TextArea
+                    onChange={(e) => {
+                      textArea(e, 4);
+                    }}
+                    placeholder="Tell us more"
+                    value={que[4]}
+                  />
+                </Form>
+              </div>
+            </div>
+          ) : null}
+
+          {count == 10 ? (
+            <div>
+              <Header>Thanks For Your Feedback</Header>
+            </div>
+          ) : null}
+        </Modal.Description>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color="black" onClick={onClose}>
+          Cancel
+        </Button>
+        {count > 3 ? (
+          <Button
+            content="Submit"
+            labelPosition="right"
+            icon="checkmark"
+            onClick={formSubmit}
+            positive
+          />
+        ) : null}
+      </Modal.Actions>
+    </Modal>
+  );
+}
--- src/mservices/contactUs.js
+++ src/mservices/contactUs.js
@@ -1,26 +1,55 @@
-import firebase from '../firebase';
-const db=firebase.firestore();
-const contactusDB=db.collection('contactus');
+import firebase from "../firebase";
+const db = firebase.firestore();
+const contactusDB = db.collection("contactus");
 
-async function contactus(details){
-    let result;
-      result=  await contactusDB.doc('contactUs').update({
-        body:firebase.firestore.FieldValue.arrayUnion(details),
-        lastModified:new Date()
-        })
-        .then(()=>{return 200})
-        .catch((err)=>{return err})
-        return result;
+async function contactus(details) {
+  let result;
+  result = await contactusDB
+    .doc("contactUs")
+    .update({
+      body: firebase.firestore.FieldValue.arrayUnion(details),
+      lastModified: new Date(),
+    })
+    .then(() => {
+      return 200;
+    })
+    .catch((err) => {
+      return err;
+    });
+  return result;
 }
 
-async function partnerRequests(details){
-    let result;
-      result=  await contactusDB.doc('partnerRequests').update({
-        body:firebase.firestore.FieldValue.arrayUnion(details),
-        lastModified:new Date()
-        })
-        .then(()=>{return 200})
-        .catch((err)=>{return err})
-        return result;
+async function partnerRequests(details) {
+  let result;
+  result = await contactusDB
+    .doc("partnerRequests")
+    .update({
+      body: firebase.firestore.FieldValue.arrayUnion(details),
+      lastModified: new Date(),
+    })
+    .then(() => {
+      return 200;
+    })
+    .catch((err) => {
+      return err;
+    });
+  return result;
 }
-export {contactus,partnerRequests}
+async function feedBack1(details) {
+  let result;
+  result = await contactusDB
+    .doc("feedBack1")
+    .update({
+      body: firebase.firestore.FieldValue.arrayUnion(details),
+      lastModified: new Date(),
+    })
+    .then(() => {
+      return 200;
+    })
+    .catch((err) => {
+      console.log(err);
+      return err;
+    });
+  return result;
+}
+export { contactus, partnerRequests, feedBack1 };
