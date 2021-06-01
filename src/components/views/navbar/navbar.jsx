df2a9344b1a29c49168aad0fdab4ff2fee150aa8
Raja Sekhar <rajasekhar.javvadi@karkinos.in>
2021-06-01 08:11:43 +0530

    navbar used states instead of doc query

--- src/components/views/navbar/navbar.jsx
+++ src/components/views/navbar/navbar.jsx
@@ -1,8 +1,7 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { NavDropdown, Navbar, Nav, Container } from "react-bootstrap";
-import { toast } from "react-toastify";
 import "./navbar.css";
 import firebase from "../../../firebase";
 import { useHistory } from "react-router-dom";
@@ -18,53 +17,24 @@ import {
   MdAddCircle,
   MdEmail,
 } from "react-icons/md";
-// import { IoMdContacts } from 'react-icons/io'
+
 import { BiLogOutCircle } from "react-icons/bi";
 import { FaCarAlt } from "react-icons/fa";
 
-firebase.auth().onAuthStateChanged(function (user) {
-  if (user) {
-    console.log("user login");
-    // document.querySelector('.userhere').innerHTML=""
-    username = "sekhar";
-    document.querySelector(".userhere").style.display = "block";
-    document.querySelector("#mychats").style.display = "block";
-    document.querySelector("#mybooks").style.display = "block";
-    document.getElementById("signup").style.display = "none";
-    document.querySelector(".userdp").style.display = "block";
-  } else {
-    console.log("user not login");
-    document.querySelector(".userhere").style.display = "none";
-    document.querySelector("#mychats").style.display = "none";
-    document.querySelector("#mybooks").style.display = "none";
-    document.querySelector(".userdp").style.display = "none";
-    document.getElementById("signup").style.display = "block";
-  }
-});
-
 const db = firebase.firestore();
-var username = "username";
-var app;
 
 function Navibar() {
   const [name, setName] = useState("undefined");
   const [pic, setpic] = useState(
     "https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"
   );
-  // const [isOpen, setIsOpen] = useState();
-  const history = useHistory();
+  const [isLogged, setisLogged] = useState(false);
 
-  // const toggleNavbar = () => {
-  //   isOpen = isNavBarOpen; //fixes next time route change where isOpen=true
-  //   isOpen = !isOpen; //fixes initial delay in calling setState(), where clicking first time didn't open NavBar
-  //   setIsOpen({
-  //     isOpen: isOpen,
-  //   });
-  //   let isNavBarOpen = isOpen;
-  // };
+  const history = useHistory();
 
   firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
+      setisLogged(true);
       db.collection("users")
         .doc(firebase.auth().currentUser.uid)
         .onSnapshot((snap) => {
@@ -73,8 +43,9 @@ function Navibar() {
             setName(snap.data().name);
             setpic(snap.data().pic);
           }
-          // setName(snap.data().name)
         });
+    } else {
+      setisLogged(false);
     }
   });
 
@@ -83,12 +54,10 @@ function Navibar() {
       .auth()
       .signOut()
       .then(function () {
-        //alert("logout successfully")
         history.push("/");
         setTimeout(() => {}, 1000);
         window.location.reload();
       });
-    // window.location.reload();
   }
 
   return (
@@ -106,124 +75,126 @@ function Navibar() {
                 </Navbar.Brand>
               </Link>
               <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-              <Navbar.Collapse
-                // onClick={toggleNavbar}
-                id="responsive-navbar-nav"
-              >
+              <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto"></Nav>
                 <Nav
                   className="nav-linkList"
                   style={{ color: "black", display: "inline-flex" }}
                 >
-                  <Link className="nav-links" to="/rentals">
-                    <Nav className="chaticon" id="mybooks">
-                      <FaCarAlt className="chaticon2" />
-                      <b>Rentals</b>
-                    </Nav>
-                  </Link>
-                  <Link className="nav-links" to="/mybookings">
-                    <Nav
-                      className="chaticon"
-                      id="mybooks"
-                      style={{
-                        display: name == "undefined" ? "none" : "block",
-                      }}
-                    >
-                      <MdWork className="chaticon2" />
-                      <b>My Bookings</b>
-                    </Nav>
-                  </Link>
-                  <Link className="nav-links" to="/chat">
-                    <Nav className="chaticon" id="mychats">
-                      <MdChatBubble className="chaticon2" />
-                      <b>Chat</b>
-                    </Nav>
-                  </Link>
+                  {isLogged ? (
+                    <>
+                      <Link className="nav-links" to="/rentals">
+                        <Nav className="chaticon" id="mybooks">
+                          <FaCarAlt className="chaticon2" />
+                          <b>Rentals</b>
+                        </Nav>
+                      </Link>
+                      <Link className="nav-links" to="/mybookings">
+                        <Nav
+                          className="chaticon"
+                          id="mybooks"
+                          style={{
+                            display: name == "undefined" ? "none" : "block",
+                          }}
+                        >
+                          <MdWork className="chaticon2" />
+                          <b>My Bookings</b>
+                        </Nav>
+                      </Link>
+                      <Link className="nav-links" to="/chat">
+                        <Nav className="chaticon" id="mychats">
+                          <MdChatBubble className="chaticon2" />
+                          <b>Chat</b>
+                        </Nav>
+                      </Link>
+                    </>
+                  ) : null}
                   <Link className="nav-links" to="/contact">
                     <Nav className="chaticon" id="contact">
                       <MdEmail size="1.3rem" className="chaticon2" />
                       <b>Contact</b>
                     </Nav>
                   </Link>
-
-                  <div
-                    className="nav-links"
-                    style={{
-                      display: "inline-flex",
-                      color: "black",
-                      marginRight: "0",
-                    }}
-                  >
-                    <img
-                      src={pic}
-                      className="userdp"
+                  {isLogged ? (
+                    <div
+                      className="nav-links"
                       style={{
-                        height: "20px",
-                        width: "20px",
-                        borderRadius: "1rem",
-                        marginTop: "10px",
-                        marginLeft: "6px",
-                        display: "none",
+                        display: "inline-flex",
+                        color: "black",
+                        marginRight: "0",
                       }}
-                    />
-
-                    <NavDropdown
-                      title={name}
-                      style={{ marginTop: "3px" }}
-                      variant="dark"
-                      id="collapsible-nav-dropdown"
-                      className="userhere"
-                      active
                     >
-                      <Link to="/account">
-                        <NavDropdown.Item href="#action/3">
-                          <MdAccountCircle color="gray" size="1.5em" />
-                          <b> Account</b>{" "}
-                        </NavDropdown.Item>
-                      </Link>
-                      <Link to="/mybookings">
-                        <NavDropdown.Item href="#action/3.1">
-                          <MdWork color="gray" size="1.5em" />
-                          <b> My Bookings</b>
-                        </NavDropdown.Item>
-                      </Link>
-                      <Link to="/chat">
-                        <NavDropdown.Item href="#action/3.2">
-                          <MdChatBubble color="gray" size="1.5em" />
-                          <b> Chats</b>
-                        </NavDropdown.Item>
-                      </Link>
-                      <Link to="/account">
-                        <NavDropdown.Item href="#action/3.3">
-                          <MdSettings color="gray" size="1.5em" />
-                          <b> Settings</b>
-                        </NavDropdown.Item>
-                      </Link>
-                      <Link to="/newpost">
-                        <NavDropdown.Item href="#action/3.4">
-                          <MdAddCircle color="gray" size="1.5em" />
-                          <b> Get Service</b>
-                        </NavDropdown.Item>
-                      </Link>
+                      <img
+                        src={pic}
+                        className="userdp"
+                        style={{
+                          height: "20px",
+                          width: "20px",
+                          borderRadius: "1rem",
+                          marginTop: "10px",
+                          marginLeft: "6px",
+                        }}
+                      />
 
-                      <NavDropdown.Divider />
-                      <NavDropdown.Item onClick={userlogout}>
-                        <BiLogOutCircle
-                          className="chaticon2"
-                          color="gray"
-                          size="1.3em"
-                        />{" "}
-                        Logout
-                      </NavDropdown.Item>
-                    </NavDropdown>
-                    {/* <Menu /> */}
-                  </div>
-                  <Link className="nav-links" to="/signup">
-                    <Nav className="chaticon" id="signup">
-                      <MdAccountCircle className="chaticon2" />
-                      <b> Signup/Login</b>
-                    </Nav>
-                  </Link>
+                      <NavDropdown
+                        title={name}
+                        style={{ marginTop: "3px" }}
+                        variant="dark"
+                        id="collapsible-nav-dropdown"
+                        className="userhere"
+                        active
+                      >
+                        <Link to="/account">
+                          <NavDropdown.Item href="#action/3">
+                            <MdAccountCircle color="gray" size="1.5em" />
+                            <b> Account</b>{" "}
+                          </NavDropdown.Item>
+                        </Link>
+                        <Link to="/mybookings">
+                          <NavDropdown.Item href="#action/3.1">
+                            <MdWork color="gray" size="1.5em" />
+                            <b> My Bookings</b>
+                          </NavDropdown.Item>
+                        </Link>
+                        <Link to="/chat">
+                          <NavDropdown.Item href="#action/3.2">
+                            <MdChatBubble color="gray" size="1.5em" />
+                            <b> Chats</b>
+                          </NavDropdown.Item>
+                        </Link>
+                        <Link to="/account">
+                          <NavDropdown.Item href="#action/3.3">
+                            <MdSettings color="gray" size="1.5em" />
+                            <b> Settings</b>
+                          </NavDropdown.Item>
+                        </Link>
+                        <Link to="/newpost">
+                          <NavDropdown.Item href="#action/3.4">
+                            <MdAddCircle color="gray" size="1.5em" />
+                            <b> Get Service</b>
+                          </NavDropdown.Item>
+                        </Link>
+
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item onClick={userlogout}>
+                          <BiLogOutCircle
+                            className="chaticon2"
+                            color="gray"
+                            size="1.3em"
+                          />{" "}
+                          Logout
+                        </NavDropdown.Item>
+                      </NavDropdown>
+                    </div>
+                  ) : null}
+                  {isLogged == false ? (
+                    <Link className="nav-links" to="/signup">
+                      <Nav className="chaticon" id="signup">
+                        <MdAccountCircle className="chaticon2" />
+                        <b> Signup/Login</b>
+                      </Nav>
+                    </Link>
+                  ) : null}
                   <Link className="nav-links" to="/newpost">
                     {" "}
                     <Nav className="chaticon">
@@ -239,8 +210,5 @@ function Navibar() {
       </header>
     </div>
   );
-  // <div className="spacediv">
-
-  // </div>
 }
 export default Navibar;
