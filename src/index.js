60ec965175bd4354c3150a530db2097a4480fe02
Raja Sekhar <rajasekhar.javvadi@karkinos.in>
2021-06-11 23:52:48 +0530

    login api integrated successfully

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
+import {createStore} from 'redux';
+import {Provider} from 'react-redux'
+import rootReducer from "./reducers/rootReducer";
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Router>
     {" "}
+    <Provider store={store}>
     <App />
+    </Provider>
   </Router>,
   document.getElementById("root")
 );
