d9acdf66b13c109a8f04268a757ffc9592b6d80a
Raja Sekhar <rajasekhar.javvadi@karkinos.in>
2021-08-15 16:06:09 +0530

    search and map placeholder still working

--- src/components/views/newpost/get_user_location.jsx
+++ src/components/views/newpost/get_user_location.jsx
@@ -6,18 +6,41 @@ import DialogContent from "@material-ui/core/DialogContent";
 import { connect } from "react-redux";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import LeafletMapp from "../leaflet/leaflet";
+import { makeStyles } from "@material-ui/core/styles";
+import "./newpost.css";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import ImageIcon from "@material-ui/icons/Image";
+import StarIcon from "@material-ui/icons/Star";
+//import icons
+import { MdLocationOn, MdLocationSearching, MdSearch } from "react-icons/md";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 function GetLocationDialog(props) {
   const [open, setOpen] = useState(false);
-
+  const classes = useStyles();
   const getUserLocation = () => {
-    if (window.navigator.geolocation) {
+    console.log("user l");
+    if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function (location) {
+        console.log("user loc", location.coords);
         props.updateCoordinates({
           lat: location.coords.latitude,
           lng: location.coords.longitude,
         });
       });
+    } else {
+      console.log("unable to find loc");
     }
   };
 
@@ -53,15 +76,62 @@ function GetLocationDialog(props) {
         </DialogTitle>
         <DialogContent>
           <>
-            <div>{props.mapAddress.display_name}</div>
-            <Button onClick={getUserLocation} color="primary">
-              Get current Location
-            </Button>
-            <LeafletMapp
-              style={{ width: "300px" }}
-              draggable={true}
-              popup={false}
-            />
+            <div className="main-content-map">
+              <div>
+                <div className="map-search-controls">
+                  <input
+                    className="input-search"
+                    placeholder="Search Street, area, colony, city.................."
+                  />
+                  {/* <MdSearch size="2.5rem" /> */}
+                  {/* <MdLocationSearching size="2.2rem" onClick={getUserLocation} /> */}
+                </div>
+                <div className="list-search-locations">
+                  <List dense={true}>
+                    <ListItem button>
+                      <div>
+                        <MdLocationOn />
+                      </div>
+                      <div className="list-text">
+                        <p>lksjfdlk</p>
+                      </div>
+                    </ListItem>
+                    <ListItem button>
+                      <ListItemIcon>
+                        <MdLocationOn />
+                      </ListItemIcon>
+                      <ListItemText primary="Chelsea Otakan" />
+                    </ListItem>
+                    <ListItem button>
+                      <ListItemIcon>
+                        <MdLocationOn />
+                      </ListItemIcon>
+                      <ListItemText primary="Eric Hoffman" />
+                    </ListItem>
+                    <ListItem button>
+                      <ListItemIcon>
+                        <MdLocationOn />
+                      </ListItemIcon>
+                      <ListItemText primary="Chelsea Otakan" />
+                    </ListItem>
+                    <ListItem button>
+                      <ListItemIcon>
+                        <MdLocationOn />
+                      </ListItemIcon>
+                      <ListItemText primary="Eric Hoffman" />
+                    </ListItem>
+                  </List>
+                </div>
+              </div>
+              {/* <div>{props.mapAddress.display_name}</div> */}
+              <div>
+                <LeafletMapp
+                  style={{ width: "260px" }}
+                  draggable={true}
+                  popup={false}
+                />
+              </div>
+            </div>
           </>
         </DialogContent>
         <DialogActions>
