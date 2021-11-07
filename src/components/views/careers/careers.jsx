c69496eb9368ff0c87200d8fb15ff611bade8874
modernsilpi <modernsilpi@gmail.com>
2021-11-07 03:33:36 +0000

    styling form

--- src/components/views/careers/careers.jsx
+++ src/components/views/careers/careers.jsx
@@ -6,6 +6,14 @@ import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 import Slider from "@mui/material/Slider";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import InputLabel from "@mui/material/InputLabel";
+import NativeSelect from "@mui/material/NativeSelect";
+import Button from "@mui/material/Button";
+import Banner from "../../../images/35_writing.png";
 
 function valuetext(value) {
   return `${value}°C`;
@@ -23,42 +31,51 @@ export default function Careers() {
         autoComplete="off"
       >
         <div className="careers-div">
-          <TextField
-            required
-            id="standard-required"
-            label="Name"
-            defaultValue="Hello World"
-            variant="standard"
-            className="TextField"
-          />{" "}
-          <br />
-          <TextField
-            required
-            id="standard-required"
-            label="Email"
-            defaultValue="Hello World"
-            variant="standard"
-            className="TextField"
-          />{" "}
-          <br />
-          <TextField
-            required
-            id="standard-required"
-            label="City/District"
-            defaultValue="Hello World"
-            variant="standard"
-            className="TextField"
-          />{" "}
-          <br />
-          <TextField
-            required
-            id="standard-required"
-            label="Contact Number"
-            defaultValue="Hello World"
-            variant="standard"
-            className="TextField"
-          />{" "}
-          <br />
+          <img src={Banner} alt="banner" className="Banner" />
+          <div className="form-card">
+            <TextField
+              required
+              id="standard-required"
+              label="Name"
+              defaultValue="Hello World"
+              variant="standard"
+              className="TextField"
+            />{" "}
+          </div>
+
+          <div className="form-card">
+            <TextField
+              required
+              id="standard-required"
+              label="Email"
+              defaultValue="Hello World"
+              variant="standard"
+              className="TextField"
+            />{" "}
+          </div>
+
+          <div className="form-card">
+            <TextField
+              required
+              id="standard-required"
+              label="City/District"
+              defaultValue="Hello World"
+              variant="standard"
+              className="TextField"
+            />{" "}
+          </div>
+
+          <div className="form-card">
+            <TextField
+              required
+              id="standard-required"
+              label="Contact Number"
+              defaultValue="Hello World"
+              variant="standard"
+              className="TextField"
+            />{" "}
+          </div>
+
           {/* <TextField
             required
             id="standard-required"
@@ -67,9 +84,9 @@ export default function Careers() {
             variant="standard"
             className="TextField"
           />{" "} */}
-          <div className="Checks">
-            <h5>Languages Known?</h5>
-            <FormGroup className="Checks TextField">
+          <div className="Checks form-card">
+            <h5 className="Labels">Languages Known?</h5>
+            <FormGroup className="Checks TextField1">
               <FormControlLabel
                 control={<Checkbox />}
                 label="HTML5"
@@ -92,7 +109,7 @@ export default function Careers() {
               />
             </FormGroup>
           </div>
-          <br />
+
           {/* <TextField
             required
             id="standard-required"
@@ -101,8 +118,10 @@ export default function Careers() {
             variant="standard"
             className="TextField"
           /> */}
-          <div className="Checks">
-            <h5>Rate yourself, How good you are in React.js?</h5>
+          <div className="Checks form-card">
+            <h5 className="Labels">
+              Rate yourself, How good you are in React.js?
+            </h5>
             <Slider
               aria-label="Small steps"
               defaultValue={10}
@@ -115,43 +134,134 @@ export default function Careers() {
               className="slider"
             />
           </div>
-          <br />
-          <TextField
+
+          {/* <TextField
             required
             id="standard-required"
             label="Months of experience in react.js"
             defaultValue="Hello World"
             variant="standard"
             className="TextField"
-          />
-          <br />
-          <TextField
+          /> */}
+          <div className="Checks form-card">
+            <FormControl component="fieldset">
+              <FormLabel component="legend">
+                Months of experience in react.js?
+              </FormLabel>
+              <RadioGroup aria-label="experience" name="radio-buttons-group">
+                <FormControlLabel
+                  value="1 to 4 months"
+                  control={<Radio />}
+                  label="1 to 4 months"
+                />
+                <FormControlLabel
+                  value="4 to 8 months"
+                  control={<Radio />}
+                  label="4 to 8 months"
+                />
+                <FormControlLabel
+                  value="8 to 12 months"
+                  control={<Radio />}
+                  label="8 to 12 months"
+                />
+                <FormControlLabel
+                  value="Above 12 months"
+                  control={<Radio />}
+                  label="Above 12 months"
+                />
+                <FormControlLabel
+                  value="other"
+                  control={<Radio />}
+                  label="Other"
+                />
+              </RadioGroup>
+            </FormControl>
+          </div>
+
+          {/* <TextField
             required
             id="standard-required"
             label="Previous experiences?"
             defaultValue="Hello World"
             variant="standard"
             className="TextField"
-          />
-          <br />
-          <TextField
-            required
-            id="standard-required"
-            label="Upload Resume"
-            defaultValue="Hello World"
-            variant="standard"
-            className="TextField"
-          />
-          <br />
-          <TextField
-            required
-            id="standard-required"
-            label="Comments"
-            defaultValue="Hello World"
-            variant="standard"
-            className="TextField"
-          />
-          <br />
+          /> */}
+          <div className="Checks form-card">
+            <h5 className="Labels">Previous Experience?</h5>
+            <FormGroup className="Checks TextField1">
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Internship"
+                className="slider"
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Job"
+                className="slider"
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Freelancer"
+                className="slider"
+              />
+
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Learning"
+                className="slider"
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="None of the above"
+                className="slider"
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Others"
+                className="slider"
+              />
+            </FormGroup>
+          </div>
+
+          <div className="form-card">
+            <FormControl fullWidth className="TextField">
+              <InputLabel variant="standard" htmlFor="uncontrolled-native">
+                Applying for?
+              </InputLabel>
+              <NativeSelect
+                defaultValue="React.js"
+                inputProps={{
+                  name: "applying",
+                  id: "uncontrolled-native",
+                }}
+              >
+                <option value="react">React.js</option>
+                <option value="flutter">Flutter</option>
+                <option value="designer">Designer</option>
+              </NativeSelect>
+            </FormControl>
+          </div>
+
+          <div className="Checks form-card">
+            <h5 className="Labels">Upload Resume</h5>
+
+            <input type="file" className="TextField1" label="Upload Resume" />
+          </div>
+
+          <div className="form-card">
+            <TextField
+              required
+              id="standard-required"
+              label="Comments"
+              defaultValue="Hello World"
+              variant="standard"
+              className="TextField"
+            />
+          </div>
+
+          <Button variant="contained" className="Submit">
+            Submit Form
+          </Button>
         </div>
       </Box>
     </div>
