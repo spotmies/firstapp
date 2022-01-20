ed92e8be4ead61960bb78f041facdcee9ae7389b
modernsilpi <modernsilpi@gmail.com>
2022-01-21 01:10:13 +0530

    working on home slides

---
+++ src/components/views/home/counts/how_it_works.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import "../style.scss";
+import {
+  AiOutlineCheckCircle,
+  AiOutlineMobile,
+  AiOutlineUsergroupAdd,
+} from "react-icons/ai";
+
+export default function HowItWorks() {
+  const data = [
+    {
+      icon: AiOutlineMobile,
+      title: "Request a Service",
+      color: "#008fdb",
+      desc: "There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form, by injected humour",
+    },
+    {
+      icon: AiOutlineUsergroupAdd,
+      title: "Connect to nearest partner",
+      color: "#008fdb",
+      desc: "There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form, by injected humour",
+    },
+    {
+      icon: AiOutlineCheckCircle,
+      title: "Get service done",
+      color: "#008fdb",
+      desc: "There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form, by injected humour",
+    },
+  ];
+
+  const children = (icon, color, title, desc, isBackground) => {
+    return (
+      <div
+        className={
+          isBackground == true ? "child1 background-container" : "child1"
+        }
+      >
+        <div className={isBackground ? "background-circle" : "circle"}>
+          <span className="icon">
+            {React.createElement(icon, { color: color, size: "3rem" })}
+          </span>
+        </div>
+        <div className="content">
+          <p className="shead">{title}</p>
+          <p className="description">{desc}</p>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+ 
+
+      
+      <div className="how-it-works">
+        <p className="head home-page-head">How it works</p>
+       <div className="spacer" />
+        <div className="parent">
+          {data.map((item, key) => {
+            return (
+              <div key={key} >
+                {children(item.icon, item.color, item.title, item.desc, true)}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+   
+  );
+}
--- src/components/views/home/home_page.jsx
+++ src/components/views/home/home_page.jsx
@@ -4,18 +4,29 @@ import Slide1 from "./slide1/slide1";
 import FooterBar from "./footer_bar/footer_bar";
 import Benefits2 from "./counts/benefits2";
 import Benefits from "./counts/benefits";
+import HowItWorks from "./counts/how_it_works";
+import Slide2 from "./slide2/slide2";
 
 export default function Homepage() {
   return (
     <div className="home-page">
-      <div className="home-slide1">
+      <div className="home-slide1 smooth-scroll">
         <Slide1 />
       </div>
-      <div className="home-slide3">
+      <div className="home-slide2 smooth-scroll">
+        <HowItWorks />
+      </div>
+      <div className="home-slide3 smooth-scroll">
         <Benefits2 />
       </div>
-      <div className="home-slide4">
+      <div className="home-slide6 center-divy view-hight smooth-scroll"> 
+        <Slide2 />
+      </div>
+      {/* <div className="home-slide4">
         <Benefits />
+      </div> */}
+      <div className="home-slide5 smooth-scroll">
+
       </div>
       <div className="home-footer">
         <FooterBar />
