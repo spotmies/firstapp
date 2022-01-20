60796eb8cf55898935747e2319d769c56baba9e5
modernsilpi <modernsilpi@gmail.com>
2022-01-20 14:47:49 +0530

    gilroy font added as text font

---
+++ src/components/views/home/slide1/slide1.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import workImage from "../../../../assets/svgs/speechtotext.svg";
+
+export default function Slide1() {
+  return (
+    <div className="center">
+      <div className="benefits2 slide-1">
+        <div className="main-content">
+          <div className="content">
+            <p className="head home-page-head">Life Should Be Easy.</p>
+            <div className="spacer-span" />
+            <div className="spacer-span" />
+            <p className="description">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text.
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text.
+            </p>
+            <div className="get-started-btn">
+              <span>
+                <p>Get started</p>
+              </span>
+            </div>
+          </div>
+          <div className="image">
+            <img src={workImage} alt="downloadApp" className="image-url" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
